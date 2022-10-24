51b82506fad001ec58a3db1d79362d9886380c97
acbuynak <44533530+acbuynak@users.noreply.github.com>
2022-10-24 13:30:40 -0400

    Init application package

---
+++ pc_application/scripts/pc_main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Software License Agreement (Apache 2.0 License)
+# Copyright (c) 2022, A.C. Buynak
+# 
+# Author: A.C. Buynak
+
+# Description:
+# System control for a pumpking carving robot!
+#
